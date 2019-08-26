9f7883f4d51d92b67f28cce8f979325671d567b8
vineet-cyberkan <vineet1.st@gmail.com>
2019-08-26 12:22:54 +0530

    vertical text slider

---
+++ custom.js
@@ -0,0 +1,125 @@
+var wahhao = window.wahhao || {};
+//var userData = [], dc = '#dataCont', child, childH, childC, dcI, item, activeSlideH, topPos, newTop, currentIndex = 0;
+var userData = [], dCont = '#dataCont';
+var dc, child, childH, childC, dcI, item, activeSlideH, topPos, newTop, currentIndex = 0; //slider var
+
+(function (window, document, $, wahhao) {
+	wahhao.guess = {
+		init: function(options){
+			var defaults = {};
+			this.options = $.extend(defaults, options);
+			wahhao.guess.dataRequest();
+		},
+
+		dataRequest: function(){
+			wahhao.guess.pageLoaderShow();
+			$.ajax({
+	            url: 'http://localhost/wahhao/guess_api.json',
+	            type: 'GET',
+	            contentType: 'application/json',
+	            dataType: 'json',
+
+	            success: function success(productsData) {
+					//console.log(productsData.status)
+					if (productsData.status == true && productsData.response.game.length > 0  ) {
+						var products = userData = productsData.response.game;
+						wahhao.guess.renderData(userData);
+					}
+	            },
+
+	            complete: function() {
+	                wahhao.guess.pageLoaderRemove();
+	            }
+        	});
+			
+		},
+
+		pageLoaderShow: function() {
+	        $('body').append('<div class="ajax-loader" style="" ></div>')
+	    },
+
+	    pageLoaderRemove: function() {
+	        $('body>.ajax-loader').remove();
+	    },
+
+	    renderData: function(userData){
+	    	if (userData.length > 0 ) {
+	    		$(dCont).empty();
+	    		var li = '';
+	    		for ( var x in userData ){
+	    			var p = '', u = '', filteredDataNode = userData[x];
+	    			for ( var nodes in filteredDataNode) {
+	    				if (nodes == "username") {
+	    					u = filteredDataNode[nodes];
+	    				}
+	    				if (nodes == "guessed_price") {
+	    					p = "<strong>guessed ₹"+filteredDataNode[nodes]+"</strong>";
+	    				}
+	    			}
+	    			li += '<li>'+u+" "+p+' img</li>';
+	    		}
+	    		$(dCont).append(li);
+	    		wahhao.guess.verticalSlide(dCont, 5);
+	    	} else{
+
+	    	}
+	    },
+
+	    verticalSlide: function(dCont, showItems) {
+	    	dc = $(dCont);
+	    	child = dc.find('li');
+	    	childH = child.outerHeight();
+	    	childHC = child.length;
+	    	dcI = childH*showItems;
+	    	dc.css({
+	    		"height":dcI,
+	    		"top":dcI - childH,
+	    	});
+
+			wahhao.guess.autoSlide();
+
+	    },
+
+	    cycleItems: function(){
+			item = child.eq(currentIndex);
+			child.removeClass('active_user')
+			item.addClass('active_user');
+			//activeSlideH = item.css('height');
+			activeSlideH = item.outerHeight();
+			topPos = dc.css('top');
+			newTop = parseInt(topPos) - parseInt(activeSlideH);
+			dc.css('top', newTop+'px');
+	    },
+
+	    autoSlide : function(){
+	    	var autoSlide = setInterval(function() {
+			  currentIndex += 1;
+			  if (currentIndex > childHC - 1) {
+			    //currentIndex = 0;
+			    clearInterval(autoSlide);
+			  } else {
+			  	wahhao.guess.cycleItems();
+			  }
+			  console.log(currentIndex);
+			}, 3000);
+
+
+	    }
+
+	}
+})(window, document, jQuery, wahhao);
+
+window.onload = function(){
+	if ($(dCont).length) {
+		wahhao.guess.init();
+	}
+}
+
+window.onresize = function(){
+	
+}
+
+
+
+
+
